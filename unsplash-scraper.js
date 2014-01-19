00a005a1c2da44fc4b724c908b7f3e11ddf706e0
Jesse Shawl <jesse@jshawl.com>
2014-01-19 13:45:32 -0500

    Use tumblr api

--- unsplash-scraper.js
+++ unsplash-scraper.js
@@ -1,25 +1,24 @@
-var request = require('request');
-var cheerio = require('cheerio');
-var url = require('url');
-var exec = require('child_process').exec;
+var request = require("request");
+var exec = require("exec-sync");
 
 var scrape = function() {
     var dlDir = './downloads';
-    var host = 'http://unsplash.com/';
+    var url = 'http://api.tumblr.com/v2/blog/unsplash.tumblr.com/posts/photo/?api_key=YOUR_API_KEY_HERE';
     //grabs all images and stores in array
-    request(host, function(err, resp, body) {
-        if (err)
-            throw err;
-        $ = cheerio.load(body);
-        $('.photo_div img').each(function(){
-            var fileUrl = $(this).attr('src');
-            var fileName = url.parse(fileUrl).pathname.split('/').pop();
-            var wget = 'wget -P ' + dlDir + ' ' + fileUrl;
-            var child = exec(wget, function(err, stdout, stderr) {
-                if (err) throw err;
-                else console.log(fileName + ' downloaded to ' + dlDir);
-            });
-        });        
+    request(url, function(err, resp, body) {
+        if (err) throw err;
+	var res = JSON.parse(body);
+	var posts = res.response.posts;
+	for ( post in posts ) {
+	  var post = posts[post];
+	  var fileUrl = post.photos[0].original_size.url;
+	  var fileName = fileUrl.substring(fileUrl.lastIndexOf('/')+1);
+	  var wget = 'wget -P ' + dlDir + ' ' + fileUrl;
+	  var child = exec(wget, function(err, stdout, stderr) {
+	    if (err)
+		throw err;
+	  });
+	  console.log(fileName + ' downloaded to ' + dlDir);
+	}
     });
-}
-scrape();
+}();
